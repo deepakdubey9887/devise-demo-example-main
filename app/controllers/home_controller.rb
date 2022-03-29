0c35539a4234e9e82245585dfc96dca675a80791
Deepak <deepak1239887@gmail.com>
2022-03-30 00:23:48 +0530

    without ajax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  before_action: autheticate user!
   def index
+  end
+  def get_user_details
+    #if user_signed_in?
+      
+    #else 
+      #redirect_to new_user_session_path
+    #end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
 
   root 'home#index'
+
+  get '/user_details' => 'home#get_user_details'
+  
+  #GET /users/
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
